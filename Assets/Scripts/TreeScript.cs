02c74a36135cccd8857a51308255d169c92f74cd
Jorge Alejos <jalejos@nearsoft.com>
2016-04-07 15:01:36 -0600

    first commit

---
+++ Assets/Scripts/TreeScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeScript : MonoBehaviour {
+	
+	public float speed;
+	public float limit;
+	public float chanceOfChange;
+	public float chanceOfSpawn;
+	public GameObject appleObject;
+	bool movingLeft;
+
+	void Update () {
+		Vector3 pos = this.transform.position;
+
+		if (movingLeft) {
+			pos.x -= speed * Time.deltaTime;
+			this.transform.position = pos;
+		} else {
+			pos.x += speed * Time.deltaTime;
+			this.transform.position = pos;
+		}
+
+
+	}
+
+	void FixedUpdate () {
+		Vector3 pos = this.transform.position;
+
+		if (pos.x >= limit || pos.x <= -limit || Random.Range(0f,1f) <= chanceOfChange)  {
+			movingLeft = !movingLeft;
+		}
+
+		if (Random.Range (0f, 1f) <= chanceOfSpawn) {
+			Instantiate (appleObject, pos, Quaternion.identity);
+		}
+	}
+
+}
