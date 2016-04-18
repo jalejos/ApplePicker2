ae2bf228f44666e81f2e6d812599272041ecebd2
Jorge Alejos <jalejos@nearsoft.com>
2016-04-18 01:08:33 -0600

    extra example

---
+++ Assets/Scripts/GateScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GateScript : MonoBehaviour {
+
+	GameObject obstacle;
+
+	void OnMouseEnter (){
+		this.transform.FindChild("GateObstacle").gameObject.SetActive (false);
+	}
+
+	void OnMouseExit() {
+		this.transform.FindChild("GateObstacle").gameObject.SetActive (true);
+	}
+}
